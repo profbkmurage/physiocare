55507ec30afae3b348847651589095063b39de2d
Prof BKMurage,PhD[A <b0708129774@gmail.com>
2025-10-27 20:43:36 +0300

    trial

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css'; // <- Import Bootstrap CSS
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // optional for Bootstrap JS components
-// import './index.css'; // your custom styles if any
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
